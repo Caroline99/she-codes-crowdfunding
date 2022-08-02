f29f6fdd4575a76240229068038e4da5fc4df870
Caroline99 <carolinejbirch@gmail.com>
2022-08-03 01:28:14 +1000

    setup complete

---
+++ src/pages/ProjectPage.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { oneProject } from '../data';
+
+function ProjectPage() {
+    return (
+        <div>
+            <h2>{oneProject.title}</h2>
+            <h3>Created at: {oneProject.date_created}</h3>
+            {/* <h3>Created at: {new Intl.DateTimeFormat('en-AU', {year: 'numeric', month: 'long', day: 'numeric'})}</h3> */}
+            <h3>{`Status: ${oneProject.is_open}`}</h3>
+            <h3>Pledges:</h3>
+            <ul>
+                {oneProject.pledges.map((pledgeData, key) => {
+                    if (pledgeData.anonymous) {
+                        return (       
+                            <li>
+                                {pledgeData.amount} from Anonymous
+                            </li>
+                        );
+
+                    }
+                    else {
+                        return (       
+                            <li>
+                                {pledgeData.amount} from {pledgeData.supporter}
+                            </li>
+                        );
+                    }
+                    
+                })}
+            </ul>
+        </div>
+    );
+}
+
+export default ProjectPage;
